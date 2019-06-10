198e0efc4372c6920212f6bb69b1a58fa0ffbdf4
sipctodamoon <47186352+sipctodamoon@users.noreply.github.com>
2019-06-10 08:41:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
 if [ -z "$MNPRIVKEY" ]; then
+    echo "generating new mn private key and ip addr"
+    cd /root/iote/IoTE-1.32-Linux
+    ./ioted -daemon
+    sleep 5s;
+    export MNPRIVKEY=`./iote-cli masternode genkey`
+    
     sed -i '$d' ~/.iotedata/iote.conf
     sed -i '$d' ~/.iotedata/iote.conf
     sed -i '$d' ~/.iotedata/iote.conf
     echo masternodeprivkey=$MNPRIVKEY >> ~/.iotedata/iote.conf
     echo externalip=`curl ifconfig.co` >> ~/.iotedata/iote.conf
     echo masternode=1 >> ~/.iotedata/iote.conf
+    echo "stopping ioted"
+    ./iote-cli stop
+    sleep 5s;
 else
-    echo "using old config"
+    sed -i '$d' ~/.iotedata/iote.conf
+    sed -i '$d' ~/.iotedata/iote.conf
+    sed -i '$d' ~/.iotedata/iote.conf
+    echo masternodeprivkey=$MNPRIVKEY >> ~/.iotedata/iote.conf
+    echo externalip=`curl ifconfig.co` >> ~/.iotedata/iote.conf
+    echo masternode=1 >> ~/.iotedata/iote.conf
 fi
 
 cat ~/.iotedata/iote.conf
